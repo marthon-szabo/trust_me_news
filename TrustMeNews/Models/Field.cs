1b67f4a133cbbc66b672fd6d5f3b64f11466d58f
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-08-11 15:51:46 +0200

    add design for navbar links

--- TrustMeNews/Models/Field.cs
+++ TrustMeNews/Models/Field.cs
@@ -8,11 +8,12 @@ namespace TrustMeNews.Models
     {
         public string headline { get; set; }
         public string standfirst { get; set; }
-
+        //author
         public string byline { get; set; }
-
         public string main { get; set; }
-        public string body { get; set; }
+        public string bodyText { get; set; }
+
+        //picture
         public string thumbnail { get; set; }
         public string trailText { get; set; }
 
