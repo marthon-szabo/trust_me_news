3fc6b5814670b85535dd01a9751bf87526c6d6dc
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-08-12 15:47:41 +0200

    rewrite js for section

--- TrustMeNews/wwwroot/js/site.js
+++ TrustMeNews/wwwroot/js/site.js
@@ -2,14 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-//let apiKey = "https://content.guardianapis.com/search?api-key=d0bd9a0e-8101-4525-8604-4ad01023d10c&order-by=newest&show-fields=all&page-size=6";
 
 function getNewsBySection() {
 
-    let genre = document.querySelector(".genre").addEventListener("click", function () {
-        apiKey = apiKey + "&section=" + genre.data.genre;
+    let apiKKey = "https://content.guardianapis.com/search?api-key=d0bd9a0e-8101-4525-8604-4ad01023d10c&order-by=newest&show-fields=all&page-size=6";
+    let apiKey;
+    let genre = document.querySelector(".clikk").addEventListener("click", function () {
+        apiKey = apiKKey + "&section=" + genre.data.genre;
         console.log(apiKey);
     })
 
-    
 }
+
+getNewsBySection();
