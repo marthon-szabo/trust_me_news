c750e0a59d5b7b9e0012f0eed0f34fbf282f9d17
Márton Szabó <makotoimx@gmail.com>
2020-08-12 17:02:51 +0200

    Add colon

--- TrustMeNews/wwwroot/js/controller.js
+++ TrustMeNews/wwwroot/js/controller.js
@@ -8,7 +8,7 @@
         const sb = document.querySelector("#inpSearch");
         sb.addEventListener("keypress", (e) => {
             if (e.key === "Enter") {
-                console.log(e.target.value)
+                console.log(e.target.value);
             }
         });
     }
