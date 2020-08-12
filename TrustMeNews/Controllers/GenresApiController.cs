bd96bd24fda2f020626483d56acd14acf13b1fd1
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-08-12 15:04:59 +0200

    add missing ;

--- TrustMeNews/Controllers/GenresApiController.cs
+++ TrustMeNews/Controllers/GenresApiController.cs
@@ -23,7 +23,7 @@ namespace TrustMeNews.Controllers
         [HttpPost]
         public async Task<IEnumerable<Result>> GetArticlesBySection(string section)
         {
-            string apiKeyBySection = $"https://content.guardianapis.com/search?api-key=d0bd9a0e-8101-4525-8604-4ad01023d10c&order-by=newest&show-fields=all&page-size=6&section={section}"
+            string apiKeyBySection = $"https://content.guardianapis.com/search?api-key=d0bd9a0e-8101-4525-8604-4ad01023d10c&order-by=newest&show-fields=all&page-size=6&section={section}";
             return await newsApiService.SendRequest(apiKeyBySection);
         }
     }
