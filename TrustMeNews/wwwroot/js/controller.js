e9d482c637bf3305cf098d682d6732b711360af6
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-08-13 20:40:43 +0200

    add fetch calls in controller.js

--- TrustMeNews/wwwroot/js/controller.js
+++ TrustMeNews/wwwroot/js/controller.js
@@ -11,7 +11,21 @@
                 fetch(`https://localhost:44313/search?content=${e.target.value}`, {
                     method: "get"
                 })
-                    .then((resp) => console.log(resp.json))
+                    .then((resp) => resp.json())
+                    .then((data) => {
+                        let articles = this.document.querySelector(".articles");
+                        articles.innerHTML = "";
+                        data.forEach(article => {
+                            articles.innerHTML += `
+                                <div class="article" id="${article.id}">
+                                    <img class="image" src="${article.fields.thumbnail}" />
+                                    <p>${article.webPublicationDate}</p>
+                                    <h5>${article.webTitle}</h5>
+                                    <p>${article.fields.trailText}</p>
+                                    <h6>${article.fields.byline}</h6>
+                                </div>`
+                        })
+                    })
             }
         });
     }
@@ -23,7 +37,22 @@
                 fetch(`https://localhost:44313/section?section=${genreId}`, {
                     method: "get"
                 })
-                    .then((resp) => console.log(resp.json))
+                    .then((resp) => resp.json())
+                    .then((data) => {
+                        let articles = this.document.querySelector(".articles");
+                        articles.innerHTML = "";
+                        data.forEach(article => {
+                            articles.innerHTML += `
+                                <div class="article" id="${article.id}">
+                                    <img class="image" src="${article.fields.thumbnail}" />
+                                    <p>${article.webPublicationDate}</p>
+                                    <h5>${article.webTitle}</h5>
+                                    <p>${article.fields.trailText}</p>
+                                    <h6>${article.fields.byline}</h6>
+                                </div>`
+                        })
+                    })
+
             })
         })
     }
@@ -35,7 +64,19 @@
                 fetch(`https://localhost:44313/article?article=${articleId}`, {
                     method: "get"
                 })
-                    .then((resp) => console.log(resp.json))
+                    .then((resp) => resp.json())
+                    .then((data) => {
+                        let articles = this.document.querySelector(".articles");
+                        articles.innerHTML = "";
+                            articles.innerHTML += `
+                                <div class="one-article" id="${data.id}">
+                                    <img class="image" src="${data.fields.thumbnail}" />
+                                    <p>${data.webPublicationDate}</p>
+                                    <h5>${data.webTitle}</h5>
+                                    <h6>${data.fields.bodyText}</h6>
+                                </div>`
+                    })
+
             })
         })
     }
