8a1689bfe65ab4c3cd87f368b457cfa00dd6b0c2
Márton Szabó <makotoimx@gmail.com>
2020-09-21 10:57:35 +0200

    Add db

---
+++ TrustMeNews/Migrations/20200921084912_newInitDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrustMeNews.Migrations
+{
+    public partial class newInitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
