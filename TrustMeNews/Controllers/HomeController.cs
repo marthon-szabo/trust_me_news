687f063d9411a41a2d63676cdc4c5a5382eed9f8
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-09-23 13:37:51 +0200

    refactor for original version

--- TrustMeNews/Controllers/HomeController.cs
+++ TrustMeNews/Controllers/HomeController.cs
@@ -2,21 +2,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TrustMeNews.Models;
+using TrustMeNews.Services;
 
 namespace TrustMeNews.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        INewsApi newsApiService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, INewsApi newsApiService)
         {
             _logger = logger;
+            this.newsApiService = newsApiService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            newsApiService = new NewsApiService();
+            string apiKey = "https://content.guardianapis.com/search?api-key=d0bd9a0e-8101-4525-8604-4ad01023d10c&order-by=newest&show-fields=all&page-size=6";
+            var model = newsApiService.SendRequest(apiKey).GetAwaiter().GetResult();
+            return View(model);
         }
 
         public IActionResult Privacy()
