c559f619b9512cee42599c0cd34c73e1f67d2ca4
Márton Szabó <makotoimx@gmail.com>
2020-09-24 14:45:33 +0200

    Add db

---
+++ TrustMeNews/Models/LayoutView.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrustMeNews.Data;
+
+namespace TrustMeNews.Models
+{
+    public struct LayoutView
+    {
+        private readonly TrustMeNewsDataContext _context;
+
+        public LayoutView(TrustMeNewsDataContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
